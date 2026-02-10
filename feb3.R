c3f143a1db34b65b06d33d94d6a0b6de826169e2
Lance Bachmeier <lanceb@ksu.edu>
2026-02-10 11:46:12 -0600

    Update feb3.R

--- feb3.R
+++ feb3.R
@@ -3,8 +3,17 @@ data("CreditCard")
 attach(CreditCard)
 Y <- majorcards
 mean(Y)
+
+## Implement OLS directly
+## Construct the regressor matrix X
 X <- cbind(1, age, dependents)
 X
 dim(X)
+
+## solve takes the inverse
+## t takes the transpose of a matrix
+## This is betahat ols
 solve(t(X) %*% X) %*% t(X) %*% Y
+
+## Built-in OLS command (safer than the manual version above)
 lm(majorcards ~ age+dependents)
